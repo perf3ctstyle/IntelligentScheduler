5e6c9ccddd2c28c44182e879eff17c3bad5c8c25
nikitatorop <torop.nikita1@gmail.com>
2021-11-26 21:59:11 +0300

    Add constants in ControllerExceptionHandler

--- src/main/java/by/bsuir/intelligentscheduler/controller/ControllerExceptionHandler.java
+++ src/main/java/by/bsuir/intelligentscheduler/controller/ControllerExceptionHandler.java
@@ -13,39 +13,36 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 @ControllerAdvice
 public class ControllerExceptionHandler extends ResponseEntityExceptionHandler {
 
+    private static final String REQUIRED_FIELDS_MISSING = "Some of the required fields were missing in the request.";
+    private static final String ILLEGAL_ARGUMENTS = "Some of the fields in the request had illegal values.";
+    private static final String RESOURCE_NOT_FOUND = "The required resource hasn't been found.";
+    private static final String RESOURCE_ALREADY_EXISTS = "Such a resource already exists.";
+
+    private static final int BAD_REQUEST_CODE = HttpStatus.BAD_REQUEST.value();
+    private static final int RESOURCE_NOT_FOUND_CODE = HttpStatus.NOT_FOUND.value();
+    private static final int CONFLICT_CODE = HttpStatus.CONFLICT.value();
+
     @ExceptionHandler(RequiredFieldsMissingException.class)
     public ResponseEntity<ExceptionResponseDto> handleRequiredFieldsMissingException() {
-        String exceptionMessage = "Some of the required fields were missing in the request.";
-        Integer exceptionInternalCode = HttpStatus.BAD_REQUEST.value();
-
-        ExceptionResponseDto exceptionResponse = new ExceptionResponseDto(exceptionMessage, exceptionInternalCode);
+        ExceptionResponseDto exceptionResponse = new ExceptionResponseDto(REQUIRED_FIELDS_MISSING, BAD_REQUEST_CODE);
         return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<ExceptionResponseDto> handleIllegalArgumentException() {
-        String exceptionMessage = "Some of the fields in the request had illegal values.";
-        Integer exceptionInternalCode = HttpStatus.BAD_REQUEST.value();;
-
-        ExceptionResponseDto exceptionResponse = new ExceptionResponseDto(exceptionMessage, exceptionInternalCode);
+        ExceptionResponseDto exceptionResponse = new ExceptionResponseDto(ILLEGAL_ARGUMENTS, BAD_REQUEST_CODE);
         return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<ExceptionResponseDto> handleResourceNotFoundException() {
-        String exceptionMessage = "The required resource hasn't been found.";
-        Integer exceptionInternalCode = HttpStatus.NOT_FOUND.value();;
-
-        ExceptionResponseDto exceptionResponse = new ExceptionResponseDto(exceptionMessage, exceptionInternalCode);
+        ExceptionResponseDto exceptionResponse = new ExceptionResponseDto(RESOURCE_NOT_FOUND, RESOURCE_NOT_FOUND_CODE);
         return new ResponseEntity<>(exceptionResponse, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(ResourceAlreadyExistsException.class)
     public ResponseEntity<ExceptionResponseDto> handleResourceAlreadyExistsException() {
-        String exceptionMessage = "Such a resource already exists.";
-        Integer exceptionInternalCode = HttpStatus.CONFLICT.value();;
-
-        ExceptionResponseDto exceptionResponse = new ExceptionResponseDto(exceptionMessage, exceptionInternalCode);
+        ExceptionResponseDto exceptionResponse = new ExceptionResponseDto(RESOURCE_ALREADY_EXISTS, CONFLICT_CODE);
         return new ResponseEntity<>(exceptionResponse, HttpStatus.CONFLICT);
     }
 }
